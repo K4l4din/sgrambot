682d783c0477e44cacf5c3c9d22bac8e9c8581d5
K4l4din <143734086+K4l4din@users.noreply.github.com>
2023-12-10 17:03:40 +0100

    Add files via upload

---
+++ Datagram/Users.js
@@ -0,0 +1,48 @@
+﻿const { DataTypes, Model } = require("sequelize");
+const { sequelizeInstance } = require("./index");
+
+class Users extends Model {}
+
+Users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ApiID: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ApiHash: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    abonnement: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    linked: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+  },
+  {
+    sequelize: sequelizeInstance,
+    timestamps: false
+  }
+);
+
+module.exports = Users;
---
+++ Datagram/index.js
@@ -0,0 +1,23 @@
+﻿const sequelize = require("sequelize");
+const sequelizeInstance = new sequelize.Sequelize({
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: "sendgram",
+    port: 3306,
+    dialect: 'mysql',
+    define: {
+        timestamps: true
+    },
+    logging: false,
+    timezone: "+02:00",
+    pool: {
+        max: 50,
+        min: 0,
+        acquire: 30000,
+        idle: 300000
+    }
+});
+
+module.exports.sequelizeInstance = sequelizeInstance;
+module.exports.Users = require('./Users').default;
